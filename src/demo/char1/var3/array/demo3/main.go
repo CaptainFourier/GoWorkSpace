a0b2530f4757590a2a63020617194a8f7831aaf7
Lebron <1144718623@qq.com>
2020-12-27 20:16:05 +0800

    1144718623@qq.com

--- src/demo/char1/var3/array/demo3/main.go
+++ src/demo/char1/var3/array/demo3/main.go
@@ -1,8 +1,11 @@
 package main
 
+import "fmt"
+
 func main() {
 	a := [3]int{1, 2, 3}
-	arraySum(a)
+	fmt.Println(arraySum(a))
+	makeSlisce()
 }
 func arraySum(x [3]int) int {
 	sum := 0
@@ -10,4 +13,11 @@ func arraySum(x [3]int) int {
 		sum = sum + v
 	}
 	return sum
+
+}
+
+func makeSlisce() {
+	b := make([]int, 2, 6)
+	c := b
+	fmt.Print(b, c)
 }
